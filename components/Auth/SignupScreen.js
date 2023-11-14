35b675c5091ed60f20faeab6b3c018723e1bfd69
UmarBakrudeen <thalaivaumar9501.s@gmail.com>
2023-11-14 17:46:40 +0530

    Sign up page created

--- components/Auth/SignupScreen.js
+++ components/Auth/SignupScreen.js
@@ -1,10 +1,103 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
-export default function SignupScreen() {
+const SignupScreen = () => {
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handlePhoneNumberChange = text => {
+    const formattedPhoneNumber = text.replace(/[^0-9]/g, '');
+    setPhoneNumber(formattedPhoneNumber);
+  };
+
+  const handleSignUp = () => {
+    console.log('Signing up with:', {
+      firstname,
+      lastname,
+      phoneNumber,
+      email,
+      password,
+    });
+  };
+
   return (
-    <View>
-      <Text>SignupScreen</Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="First Name"
+        onChangeText={text => setFirstname(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Last Name"
+        onChangeText={text => setLastname(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your phone number"
+        value={phoneNumber}
+        onChangeText={handlePhoneNumberChange}
+        keyboardType="phone-pad"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={text => setEmail(text)}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={text => setPassword(text)}
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+        <View>
+          <Text style={{textAlign: 'center', color: 'white'}}>Sign Up</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingLeft: 8,
+  },
+  button: {
+    paddingHorizontal: 60,
+    paddingVertical: 8,
+    borderColor: '#004600',
+    marginTop: 10,
+    borderRadius: 5,
+    backgroundColor: '#005600',
+    color: 'white',
+  },
+});
+
+export default SignupScreen;
