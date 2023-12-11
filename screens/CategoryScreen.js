38e0234b612b3df5d7d84719b11b5155f154822c
UmarBakrudeen <thalaivaumar9501.s@gmail.com>
2023-12-11 19:02:48 +0530

    card UI changed

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -6,22 +6,43 @@ import {
   FlatList,
   Image,
   TouchableOpacity,
+  useWindowDimensions,
+  Pressable,
 } from 'react-native';
 import {PRODUCTS} from '../constants';
 
 const CategoryScreen = ({route}) => {
+  const window = useWindowDimensions();
+
   const {selectedCategory} = route.params;
 
   const filteredProducts = PRODUCTS.filter(
     product => product.category === selectedCategory,
   );
 
+  const CARD_SIZE = (window.width - 2 * 20 - 20 - 10) / 2;
+
   const renderItem = ({item}) => (
-    <TouchableOpacity style={styles.productCard} onPress={() => {}}>
-      <Image source={item.Img} style={styles.productImage} />
+    <View style={[styles.productCard, {width: CARD_SIZE}]} onPress={() => {}}>
+      <View style={{alignItems: 'center'}}>
+        <Image
+          source={item.Img}
+          style={{
+            width: CARD_SIZE - 20,
+            height: CARD_SIZE - 20,
+            borderRadius: 5,
+          }}
+        />
+      </View>
       <Text style={styles.productName}>{item.name}</Text>
-      <Text style={styles.productId}>ID: {item.id}</Text>
-    </TouchableOpacity>
+      <Text style={styles.Qty}>{item.Qty}</Text>
+      <View style={styles.priceContainer}>
+        <Text style={styles.price}>Rs. {item.pricePerKg}</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>ADD</Text>
+        </Pressable>
+      </View>
+    </View>
   );
 
   return (
@@ -29,6 +50,7 @@ const CategoryScreen = ({route}) => {
       <Text style={styles.categoryTitle}>Category: {selectedCategory}</Text>
       <FlatList
         data={filteredProducts}
+        numColumns={2}
         keyExtractor={item => item.id.toString()}
         renderItem={renderItem}
         horizontal={false}
@@ -48,31 +70,56 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   productCard: {
+    paddingVertical: 10,
+    marginHorizontal: 10,
     backgroundColor: 'white',
     borderRadius: 10,
-    margin: 10,
-    padding: 16,
-    width: '48%',
+
     shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
     shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    flex: 1,
+    shadowRadius: 3.5,
+    elevation: 4,
   },
-  productImage: {
-    width: '100%',
-    height: 200,
+  button: {
+    paddingHorizontal: 25,
+    paddingVertical: 6,
+    borderWidth: 0.2,
+    borderColor: '#004600',
     borderRadius: 5,
+    backgroundColor: '#e8f5e9',
+  },
+  priceContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
   },
   productName: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: 8,
+    fontFamily: 'Lato-Bold',
+    color: '#005600',
+    fontSize: 13,
+    marginVertical: 5,
+    marginLeft: 10,
   },
-  productId: {
-    fontSize: 14,
-    color: 'gray',
+  buttonText: {
+    color: '#004600',
+    fontFamily: 'Lato-Bold',
+    fontSize: 12,
+  },
+  price: {
+    fontFamily: 'Lato-Regular',
+    fontSize: 13,
+  },
+  Qty: {
+    fontFamily: 'Lato-Bold',
+    color: '#005600',
+    fontSize: 10,
+    marginVertical: 2,
+    marginLeft: 10,
   },
 });
 
