46bde456f6328f6e457da90755cf1d1a9201ac1a
UmarBakrudeen <thalaivaumar9501.s@gmail.com>
2023-12-31 18:40:55 +0530

    category screen ui changed

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -35,13 +35,17 @@ const CategoryScreen = ({route}) => {
         />
       </View>
       <Text style={styles.productName}>{item.name}</Text>
-      <View style={styles.priceContainer}>
+      {/* <View style={styles.priceContainer}>
         <Text style={styles.Qty}>{item.Qty}</Text>
         <Text style={styles.price}>Rs. {item.pricePerKg}</Text>
       </View>
       <Pressable style={styles.button}>
         <Text style={styles.buttonText}>ADD</Text>
-      </Pressable>
+      </Pressable> */}
+      <View style={styles.row}>
+        <Text style={styles.qty}>{item.Qty}</Text>
+        <Text style={styles.price}>Rs. {item.pricePerKg}</Text>
+      </View>
     </View>
   );
 
@@ -85,46 +89,65 @@ const styles = StyleSheet.create({
     shadowRadius: 3.5,
     elevation: 4,
   },
-  button: {
-    paddingHorizontal: 25,
-    paddingVertical: 6,
-    borderWidth: 0.5,
-    borderColor: '#004600',
-    borderRadius: 5,
-    backgroundColor: '#e8f5e9',
-    marginVertical: 5,
-    marginHorizontal: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  priceContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingHorizontal: 10,
-  },
+  // button: {
+  //   paddingHorizontal: 25,
+  //   paddingVertical: 6,
+  //   borderWidth: 0.5,
+  //   borderColor: '#004600',
+  //   borderRadius: 5,
+  //   backgroundColor: '#e8f5e9',
+  //   marginVertical: 5,
+  //   marginHorizontal: 10,
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  // priceContainer: {
+  //   flexDirection: 'row',
+  //   alignItems: 'center',
+  //   justifyContent: 'space-between',
+  //   paddingHorizontal: 10,
+  // },
   productName: {
     fontFamily: 'Lato-Bold',
     color: '#005600',
-    fontSize: 13,
-    marginVertical: 5,
+    fontSize: 15,
     marginLeft: 10,
-  },
-  buttonText: {
-    color: '#004600',
-    fontFamily: 'Lato-Bold',
-    fontSize: 12,
+    marginTop: 10,
     fontWeight: 'bold',
+    height: 45,
+  },
+  // buttonText: {
+  //   color: '#004600',
+  //   fontFamily: 'Lato-Bold',
+  //   fontSize: 12,
+  //   fontWeight: 'bold',
+  // },
+  // price: {
+  //   fontFamily: 'Lato-Regular',
+  //   fontSize: 13,
+  // },
+  // Qty: {
+  //   fontFamily: 'Lato-Bold',
+  //   color: '#005600',
+  //   fontSize: 10,
+  //   marginVertical: 2,
+  // },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 10,
+  },
+  qty: {
+    fontFamily: 'Lato-Regular',
+    color: '#005600',
+    fontSize: 11,
+    marginVertical: 2,
   },
   price: {
-    fontFamily: 'Lato-Regular',
-    fontSize: 13,
-  },
-  Qty: {
     fontFamily: 'Lato-Bold',
+    fontSize: 13,
     color: '#005600',
-    fontSize: 10,
-    marginVertical: 2,
+    fontWeight: 'bold',
   },
 });
 
